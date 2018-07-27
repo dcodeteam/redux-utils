fece33c17cbe65af6254c8f3b3c3e5999b3d0556
Umidbek Karimov <uma.karimov@gmail.com>
2018-07-27 15:31:54 +0500

    test(index.spec): Test exported namespace snapshot.

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,43 +1,7 @@
-import {
-  ReducerBuilder,
-  createAsyncRequestReducer,
-  createAsyncValueDictReducer,
-  createAsyncValueReducer,
-  createFulfillAction,
-  createPerformAction,
-  createRejectAction,
-  getAsyncValueError,
-  getAsyncValuePayload,
-  getInitialAsyncRequest,
-  getInitialAsyncValue,
-  isAsyncValueFetching,
-  isFulfillAction,
-  isPerformAction,
-  isRejectAction,
-  mapToAction,
-} from "../index";
+import * as index from "..";
 
 describe("index", () => {
   test("public api", () => {
-    expect(typeof isPerformAction).toBe("function");
-    expect(typeof createPerformAction).toBe("function");
-    expect(typeof isFulfillAction).toBe("function");
-    expect(typeof createFulfillAction).toBe("function");
-    expect(typeof isRejectAction).toBe("function");
-    expect(typeof createRejectAction).toBe("function");
-
-    expect(typeof getInitialAsyncRequest).toBe("function");
-    expect(typeof createAsyncRequestReducer).toBe("function");
-
-    expect(typeof getAsyncValueError).toBe("function");
-    expect(typeof getAsyncValuePayload).toBe("function");
-    expect(typeof isAsyncValueFetching).toBe("function");
-    expect(typeof getInitialAsyncValue).toBe("function");
-    expect(typeof createAsyncValueReducer).toBe("function");
-    expect(typeof createAsyncValueDictReducer).toBe("function");
-
-    expect(typeof mapToAction).toBe("function");
-
-    expect(typeof ReducerBuilder).toBe("function");
+    expect(index).toMatchSnapshot();
   });
 });
